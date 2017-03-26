3312f9f77add2a8a71b29ea4c24c2629496d3edb
Christian Pena <christianpena9@gmail.com>
2017-03-25 23:00:26 -0400

    able to know if there is any records in the collection

--- server/main.js
+++ server/main.js
@@ -1,7 +1,18 @@
 // Only execuded on the server
 import { Meteor } from 'meteor/meteor';
+import { Employees } from '../imports/collections/employees';
 
 Meteor.startup(() => {
     // Great place to generate data
-    
+
+    // Check to see if data exists in the collection
+
+    // See if the collection has any records
+    // .find takes an empty object since we do not want to filter the find by anything
+    const = numberRecords = Employees.find({}).count();
+    if(!numberRecords) {
+        // Generate some data...
+
+    }
+
 });
